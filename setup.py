b53ac12970f41f6a65d623917be1b117ee6a8eba
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-08-25 00:19:38 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@
 from setuptools import setup, find_packages
 
 
-setup(name='my_program',
+setup(name='KostalPikoPy',
       version='0.0.1',
-      author='My Name',
-      author_email='my@mail.com',
-      url='http://www.my_program.org',
-      download_url='http://www.my_program.org/files/',
-      description='Short description of my_program...',
-      long_description='Short description of my_program...',
+      author='Christian Stade-Schuldt',
+      author_email='stadeschuldt@gmail.com',
+      url='https://github.com/Tafkas/KostalPikoPy',
+      download_url='https://github.com/Tafkas/KostalPikoPy',
+      description='A package for working with a PIKO Inverter from KOSTAL',
+      long_description='A package for working with a PIKO Inverter from KOSTAL',
 
-      packages = find_packages(),
-      include_package_data = True,
-      package_data = {
-        '': ['*.txt', '*.rst'],
-        'my_program': ['data/*.html', 'data/*.css'],
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={
+          '': ['*.txt', '*.rst'],
+          'my_program': ['data/*.html', 'data/*.css'],
       },
-      exclude_package_data = { '': ['README.txt'] },
-      
-      scripts = ['bin/my_program'],
-      
+      exclude_package_data={'': ['README.txt']},
+
+      scripts=['bin/my_program'],
+
       keywords='python tools utils internet www',
       license='GPL',
       classifiers=['Development Status :: 5 - Production/Stable',
@@ -33,8 +33,8 @@ setup(name='my_program',
                    'License :: OSI Approved :: GNU Affero General Public License v3',
                    'Topic :: Internet',
                    'Topic :: Internet :: WWW/HTTP',
-                  ],
-                  
-      #setup_requires = ['python-stdeb', 'fakeroot', 'python-all'],
-      install_requires = ['setuptools'],
-     )
+                   ],
+
+      # setup_requires = ['python-stdeb', 'fakeroot', 'python-all'],
+      install_requires=['setuptools'],
+)
