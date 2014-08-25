2b8322f5bdaa2dad143b12ff46287c4d0863b65c
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2014-08-25 21:32:22 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pikopy',
-      version='0.0.1',
+      version='0.9.0',
       author='Christian Stade-Schuldt',
       author_email='stadeschuldt@gmail.com',
       url='https://github.com/Tafkas/KostalPikoPy',
@@ -17,20 +17,19 @@ setup(name='pikopy',
       include_package_data=True,
       package_data={
           '': ['*.txt', '*.rst'],
-          'my_program': ['data/*.html', 'data/*.css'],
+          'pikopy': ['data/*.html', 'data/*.css'],
       },
       exclude_package_data={'': ['README.txt']},
 
-      scripts=['bin/my_program'],
+      #scripts=['bin/my_program'],
 
-      keywords='python tools utils internet www',
+      keywords='python kostal piko inverter photovoltaic solar',
       license='GPL',
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Natural Language :: English',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2',
-                   'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-                   'License :: OSI Approved :: GNU Affero General Public License v3',
+                   'License :: OSI Approved :: MIT License',
                    'Topic :: Internet',
                    'Topic :: Internet :: WWW/HTTP',
                    ],
